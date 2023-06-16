730bfbee157ce8a96aec42c235fa1e6012acd867
min-hyo-2k <mrrobotforwork@gmail.com>
2023-06-16 23:32:05 +0700

    Fix s3 service in S3 module

--- modules/aws/s3/main.tf
+++ modules/aws/s3/main.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "main" {
   bucket_prefix = var.name
-  acl    = "private"
+  acl           = "private"
   force_destroy = true
 
   dynamic "server_side_encryption_configuration" {
-    for_each =  [] 
+    for_each = []
 
     content {
       rule {
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "main" {
   }
 
   dynamic "logging" {
-    for_each =  [] 
+    for_each = []
 
     content {
       target_bucket = aws_s3_bucket.logging[0].id
@@ -25,25 +25,47 @@ resource "aws_s3_bucket" "main" {
   }
 
   versioning {
-      enabled =  false 
-      mfa_delete = false
+    enabled    = true
+    mfa_delete = false
   }
 
   dynamic "website" {
-    for_each =  [] 
+    for_each = []
 
     content {
       index_document = "index.html"
     }
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "arn"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = "target-bucket"
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main[0].bucket_prefix
+
+  restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
 }
 
 resource "aws_s3_bucket" "logging" {
   bucket_prefix = var.name
-  acl    = var.bucket_acl
+  acl           = var.bucket_acl
   force_destroy = true
 
-  count =  0 
+  count = 0
 }
 
 data "aws_iam_policy_document" "force_ssl_only_access" {
@@ -77,7 +99,7 @@ resource "aws_s3_bucket_policy" "force_ssl_only_access" {
   bucket = aws_s3_bucket.main.id
   policy = data.aws_iam_policy_document.force_ssl_only_access.json
 
-  count = 1 
+  count = 1
 }
 
 data "aws_iam_policy_document" "getonly" {
@@ -105,6 +127,32 @@ resource "aws_s3_bucket" "getonly" {
   force_destroy = true
 
   count = 1
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "arn"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = "target-bucket"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "getonly" {
+  bucket = aws_s3_bucket.getonly[0].bucket_prefix
+
+  restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
 }
 
 data "aws_iam_policy_document" "public" {
@@ -130,5 +178,31 @@ data "aws_iam_policy_document" "public" {
 resource "aws_s3_bucket" "public" {
   bucket_prefix = "sadcloudhetonlys3"
   force_destroy = true
-  count = 1
+  count         = 1
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "arn"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = "target-bucket"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "public" {
+  bucket = aws_s3_bucket.public[0].bucket_prefix
+
+  restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
 }
